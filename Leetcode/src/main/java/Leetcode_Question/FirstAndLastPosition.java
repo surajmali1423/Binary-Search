5a2309363890e6aa4fd5c058cb879329b9c4ba09
surajmali1423 <malisuraj1423@gmail.com>
2026-08-19 15:02:38 +0530

    2089. Find Target Indices After Sorting Array :
    You are given a 0-indexed integer array nums and a target element target.
    
    A target index is an index i such that nums[i] == target.
    
    Return a list of the target indices of nums after sorting nums in non-decreasing order. If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
    
    Example 1:
    
    Input: nums = [1,2,5,2,3], target = 2
    Output: [1,2]
    Explanation: After sorting, nums is [1,2,2,3,5].
    The indices where nums[i] == 2 are 1 and 2.
    Example 2:
    
    Input: nums = [1,2,5,2,3], target = 3
    Output: [3]
    Explanation: After sorting, nums is [1,2,2,3,5].
    The index where nums[i] == 3 is 3.
    Example 3:
    
    Input: nums = [1,2,5,2,3], target = 5
    Output: [4]
    Explanation: After sorting, nums is [1,2,2,3,5].
    The index where nums[i] == 5 is 4.
    
    Constraints:
    
    1 <= nums.length <= 100
    1 <= nums[i], target <= 100
    
    Link : https://leetcode.com/problems/find-target-indices-after-sorting-array/

--- Leetcode/src/main/java/Leetcode_Question/FirstAndLastPosition.java
+++ Leetcode/src/main/java/Leetcode_Question/FirstAndLastPosition.java
@@ -44,18 +44,56 @@ public class FirstAndLastPosition {
         return ans;
     }
 
-    static int [] FirstAndLastElement(int [] arr , int target){
+    static int [] FirstAndLastElement1(int [] arr , int target){
          int first = FirstElement(arr , target);
          int last = LastPosition(arr , target);
 
          return new int[]{first , last};
     }
 
+    static int [] FirstAndLastElement2(int [] arr , int target){
+          int lo = 0;
+          int hi = arr.length - 1;
+          int first = -1;
+
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+
+            if(arr[mid] == target){
+                first = mid;
+                hi = mid - 1;
+            }
+            else if(target < arr[mid]) hi = mid - 1;
+            else lo = mid + 1;
+        }
+
+          lo = 0;
+          hi = arr.length - 1;
+          int second = -1;
+
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+
+            if(arr[mid] == target){
+                second = mid;
+                lo = mid + 1;
+            }
+            else if(target < arr[mid]) hi = mid - 1;
+            else lo = mid + 1;
+        }
+
+        return new int[]{first , second};
+    }
+
+    static int Occurance(int [] ans){
+         return ans[1] - ans[0] + 1;
+    }
     static void main(String[] args) {
-        int [] arr = { 5 , 7 , 7 , 8 ,8 , 10};
+        int [] arr = { 5 , 7 , 7  , 8 ,8 , 8, 10};
         int target = 7;
 
-        int [] ans = FirstAndLastElement(arr , target);
+        int [] ans = FirstAndLastElement2(arr , target);
+
         for(int ele : ans){
             System.out.print(ele+" ");
         }
