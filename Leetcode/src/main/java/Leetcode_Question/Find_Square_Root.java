330d3f83357d5ff283691227ceeb5b1989f205e5
surajmali1423 <malisuraj1423@gmail.com>
2026-08-24 11:34:41 +0530

    69. Sqrt(x) : Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
    You must not use any built-in exponent function or operator.
    
    For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
    
    Example 1:
    
    Input: x = 4
    Output: 2
    Explanation: The square root of 4 is 2, so we return 2.
    Example 2:
    
    Input: x = 8
    Output: 2
    Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
    
    Constraints:
    
    0 <= x <= 231 - 1
    
    Link :https://leetcode.com/problems/sqrtx/

---
+++ Leetcode/src/main/java/Leetcode_Question/Find_Square_Root.java
@@ -0,0 +1,31 @@
+package Leetcode_Question;
+
+public class Find_Square_Root {
+
+    static int sqrt(int x){
+         int ans = 1;
+
+         int lo = 1;
+         int hi = x;
+
+         while (lo <= hi){
+              int mid = lo + (hi - lo) / 2;
+              int root = mid * mid;
+
+               if(root <= x){
+                    ans = mid;
+                    lo = mid + 1;
+               }
+               else {
+                   hi = mid - 1;
+               }
+         }
+
+//          Return the hi also Both Are point the same name ;
+         return ans;
+    }
+    static void main() {
+        int squareroot = 50;
+        System.out.println("The Closed Square Of the "+squareroot+ " is "+sqrt(squareroot));
+    }
+}
